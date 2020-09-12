53b38306a278794b4fa930fa7f82c6f231296f5c
gsoukup <gabor.soukup@gmail.com>
2020-09-12 20:32:13 +0200

    Removing BS

--- folders.py
+++ folders.py
@@ -1,17 +1,3 @@
 #!usr/bin/env/ python3
 #-*- Coding:utf-8 -*-
-from pathlib import Path
-import os
-from calc import makemath
 
-myPath = r"C:\Users\urbansamurai\Desktop\TELTRANS_lostest\Helyszinek"
-path = Path.cwd()
-
-"""
-
-with open("folders.txt")
-
-"""
-
-print(f"My path is:{path}")
-makemath(5,6)
