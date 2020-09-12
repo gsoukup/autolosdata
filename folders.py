f14e5114673cf911639a4e91744c1e8e8f0b1727
gsoukup <gabor.soukup@gmail.com>
2020-09-12 20:25:27 +0200

    Creating files for the folder creating process

---
+++ folders.py
@@ -0,0 +1,17 @@
+#!usr/bin/env/ python3
+#-*- Coding:utf-8 -*-
+from pathlib import Path
+import os
+from calc import makemath
+
+myPath = r"C:\Users\urbansamurai\Desktop\TELTRANS_lostest\Helyszinek"
+path = Path.cwd()
+
+"""
+
+with open("folders.txt")
+
+"""
+
+print(f"My path is:{path}")
+makemath(5,6)
